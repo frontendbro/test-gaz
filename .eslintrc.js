7a068a90198f602abbae4e3324f94166d68d68e2
Семён Задорожный <simon.z@yandex.ru>
2021-02-06 07:27:53 +0300

    first commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,22 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/essential", "@vue/prettier"],
   parserOptions: {
     parser: "babel-eslint"
   },
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      "warn",
+      {
+        "semi": false,
+        "singleQuote": true,
+        "printWidth": 100,
+        "tabWidth": 2,
+        "arrowParens": "avoid"
+      }
+    ]
   }
 };
